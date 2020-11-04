a7ff63767332087c4c9490d3cd5fee8a8d4c2ebc
t-gott <70244874+t-gott@users.noreply.github.com>
2020-11-04 15:11:00 -0600

    Edited the UI to be akin to that of iOS's calculator application

--- app/src/main/java/com/example/calculator/Calculator.java
+++ app/src/main/java/com/example/calculator/Calculator.java
@@ -7,129 +7,136 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Calculator extends AppCompatActivity {
 
-    private String currentValue = "";
+    private String dispValue = ""; //what is displayed on the output screen
     private String lastValue = "";
+    private double currentValue = 0;
+    private double tempValue = 0;
     private String operation = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.calculator);
     }
 
-    private void insertNumber(int num) {
-        currentValue += num;
-        setDisplay(currentValue);
+    private void insertNum(int num) {
+        dispValue += num;
+        setDisp(dispValue);
     }
 
-    private void setDisplay(String str) {
+    private void setDisp(String str) {
         TextView view = (TextView)findViewById(R.id.displayValue);
         view.setText(str);
     }
 
     public void onNum1(View view) {
-        insertNumber(1);
+        insertNum(1);
     }
 
     public void onNum2(View view) {
-        insertNumber(2);
-    } //changed insertNumber(4) to insertNumber(2)
+        insertNum(2);
+    } //changed insertNum(4) to insertNum(2)
 
     public void onNum3(View view) {
-        insertNumber(3);
+        insertNum(3);
     }
 
     public void onNum4(View view) {
-        insertNumber(4);
+        insertNum(4);
     }
 
     public void onNum5(View view) {
-        insertNumber(5);
+        insertNum(5);
     }
 
     public void onNum6(View view) {
-        insertNumber(6);
+        insertNum(6);
     }
 
     public void onNum7(View view) {
-        insertNumber(7);
+        insertNum(7);
     }
 
     public void onNum8(View view) {
-        insertNumber(8);
-    } //changed insertNumber(0) to insertNumber(8)
+        insertNum(8);
+    } //changed insertNum(0) to insertNum(8)
 
     public void onNum9(View view) {
-        insertNumber(9);
+        insertNum(9);
     }
 
     public void onNum0(View view) {
-        insertNumber(0);
+        insertNum(0);
     }
 
     public void onAdd(View view) {
-        lastValue = currentValue;
-        currentValue = ""; //clears the current value to allow for a new input
+        lastValue = dispValue;
+        dispValue = ""; //clears the current value to allow for a new input
         operation = "add";
     }
 
     public void onSubtract(View view) {
-        lastValue = currentValue;
-        currentValue = ""; //clears the current value to allow for a new input
+        lastValue = dispValue;
+        dispValue = ""; //clears the current value to allow for a new input
         operation = "subtract";
     }
 
     public void onMultiply(View view) {
-        lastValue = currentValue;
-        currentValue = ""; //clears the current value to allow for a new input
+        lastValue = dispValue;
+        dispValue = ""; //clears the current value to allow for a new input
         operation = "multiply";
     }
 
     public void onDivide(View view) {
-        lastValue = currentValue;
-        currentValue = ""; //clears the current value to allow for a new input
+        lastValue = dispValue;
+        dispValue = ""; //clears the current value to allow for a new input
         operation = "divide";
     }
 
+    public void onPercent(View view) {
+        lastValue = dispValue;
+
+    }
+
     public void onEquals(View view) {
-        if(lastValue.length() > 0 && currentValue.length() > 0) {
+        if(lastValue.length() > 0 && dispValue.length() > 0) {
             int val1 = Integer.parseInt(lastValue);
-            int val2 = Integer.parseInt(currentValue);
+            int val2 = Integer.parseInt(dispValue);
 
             int newValue = 0;
             switch (operation) {
                 case "add":
                     newValue = val1 + val2;
-                    setDisplay(Integer.toString(newValue));
+                    setDisp(Integer.toString(newValue));
                     break;
                 case "subtract":
                     newValue = val1 - val2; //changed '+' to '-'
-                    setDisplay(Integer.toString(newValue));
+                    setDisp(Integer.toString(newValue));
                     break;
                 case "multiply":
                     newValue = val1 * val2; //changed '/' to '*'
-                    setDisplay(Integer.toString(newValue));
+                    setDisp(Integer.toString(newValue));
                     break;
                 case "divide": //changed from "divider" to "divide"
                     if (val2 == 0) {
-                        setDisplay("Error: Div by 0");; //moved setDisplay to each case to prevent division by 0
+                        setDisp("Error: Div by 0");; //moved setDisplay to each case to prevent division by 0
                     }
                     else {
                         newValue = val1 / val2;
-                        setDisplay(Integer.toString(newValue));
+                        setDisp(Integer.toString(newValue));
                     }
                     break;
             }
-            //clears last and currentValues for next input
+            //clears last and dispValues for next input
             lastValue = "";
-            currentValue = "";
+            dispValue = "";
         }
     }
 
     public void onClear(View view) {
         operation = "";
-        currentValue = "";
+        dispValue = "";
         lastValue = "";
-        setDisplay(""); //added to clear the display when clear is pressed
+        setDisp(""); //added to clear the display when clear is pressed
     }
 }
